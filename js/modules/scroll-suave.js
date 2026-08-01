9bb43691717ae7c99b528fc47da04052f44db204
Lucas Dias <lucass.diass23@gmail.com>
2026-08-01 03:35:00 -0300

    refactor: modulação dos arquivos de script.

---
+++ js/modules/scroll-suave.js
@@ -0,0 +1,22 @@
+export default initScrollSuave();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll(
+    '[data-menu="suave"] a[href^="#"]',
+  );
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
