037880e14bae61ae711c66012aaf1566238f2c60
Lucas Dias <lucass.diass23@gmail.com>
2026-08-02 23:28:26 -0300

    feat: adição da seção números com animação e ícone favicon

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -13,3 +14,4 @@ initTabNav();
 initModal();
 initDropdownMenu();
 initMenuMobile();
+initAnimaNumeros();
