d6b0b1f6ab188fa8734c71e45fea09c7f1773405
Lucas Dias <lucass.diass23@gmail.com>
2026-08-06 12:11:06 -0300

    feat: adição fetch API JSON no fetch.bitcoin.js e JS assincrono no fetch-animais para animação de números

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -14,4 +16,6 @@ initTabNav();
 initModal();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
